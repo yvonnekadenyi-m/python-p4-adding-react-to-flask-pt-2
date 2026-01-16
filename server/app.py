7feb3b68bf8f0be114ac9d5d23eabbbd2a156339
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2026-01-16 17:18:34 +0300

    added to app.js

--- server/app.py
+++ server/app.py
@@ -1,18 +1,33 @@
-
-from flask import Flask, make_response, jsonify
+from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
+from flask_migrate import Migrate
+
+from models import db, Movie
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
 
 CORS(app)
+migrate = Migrate(app, db)
+
+db.init_app(app)
 
 @app.route('/movies', methods=['GET'])
 def movies():
-    response_dict = {
-        "text": "Movies will go here"
-    }
+    if request.method == 'GET':
+        movies = Movie.query.all()
 
-    return make_response(jsonify(response_dict), 200)
+        return make_response(
+            jsonify([movie.to_dict() for movie in movies]),
+            200,
+        )
+
+    return make_response(
+        jsonify({"text": "Method Not Allowed"}),
+        405,
+    )
 
 if __name__ == '__main__':
     app.run(port=5555)
